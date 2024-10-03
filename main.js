b948ddf110d9dc4b1af26a5f9bcb29e38c2326e0
HasanSFU <hmjafari@gmail.com>
2024-10-03 14:32:18 -0700

    image1-3

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-window.onload = function() {
-    alert("Welcome to my personal website!");
-};
+document.querySelectorAll('.image-container img').forEach(img => {
+    img.addEventListener('mouseover', function() {
+        this.style.transform = 'scale(1.1)';
+        this.style.transition = 'transform 0.3s';
+    });
+    img.addEventListener('mouseout', function() {
+        this.style.transform = 'scale(1)';
+    });
+});
