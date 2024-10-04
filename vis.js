7b2ccd46fe5329bb94385f4e3bf63ee4d85dca0f
HasanSFU <hmjafari@gmail.com>
2024-10-03 17:27:50 -0700

    keep older version of website

--- vis.js
+++ vis.js
@@ -6,3 +6,7 @@ newCircle.setAttribute("cy", "150");
 newCircle.setAttribute("r", "40");
 newCircle.setAttribute("fill", "yellow");
 svg.appendChild(newCircle);
+
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+console.log(d3)
+
---
+++ vistest.js
@@ -0,0 +1,4 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+console.log(d3)
+d3.select('#"visContainer"').append("p").text("hello, world");
